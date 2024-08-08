10b2cf5d7dabee0a86b3686cc3711c6db1753680
Super User <101892232+i-douk@users.noreply.github.com>
2024-08-08 11:37:08 +0100

    13.13

--- app/controllers/blogs.js
+++ app/controllers/blogs.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const { Blog, User } = require('../models')
 const { sequelize } = require('../util/db')
 const { SECRET } = require('../util/config')
-
+const { Op } = require('sequelize')
 const main = async () => {
         await sequelize.authenticate()
         const blogs = await Blog.findAll()
@@ -34,16 +34,23 @@ const tokenExtractor = (req, res, next) => {
     next()
   }
 
-router.get('/', async (req, res) => {
-    const blogs = await Blog.findAll({
-        attributes: { exclude: ['userId'] },
-        include: {
-          model: User,
-          attributes: ['name']
-        }
+router.get('/', async (req, res) => { 
+  const where = {}
+    if (req.query.search) {
+      where.title = {
+        [Op.iLike]: `%${req.query.search}%`
       }
-    )
-    res.json(blogs)
+  }
+
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ['userId'] },
+    include: {
+      model: User,
+      attributes: ['name']
+    },
+    where
+  })
+  res.json(blogs)
 })
 
 router.post('/', tokenExtractor ,async (req, res) => {
